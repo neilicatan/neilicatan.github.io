a485393bea784717bede70a73fc977a28f227472
neilicatan <nhdrlicatan@usm.edu.ph>
2024-11-26 15:23:46 +0800

    new file:   index.html 	modified:   lib/pages/index.php

--- lib/pages/index.php
+++ lib/pages/index.php
@@ -154,20 +154,21 @@ $indexHouses = new Index();
             <div class="lg:col-span-4 rounded-xl space-y-8 bg-white text-center py-8 px-4 dark:bg-slate-900 sm:col-span-6">
                 <div class="space-y-1.5">
                     <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae similique animi rem commodi reprehenderit
-                        numquam,
-                        ipsa nostrum vel nesciunt facilis.
+                        nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
+                        nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
+                        nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
                     </p>
 
                     <p>
-                        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae similique animi rem commodi reprehenderit
-                        numquam,
-                        ipsa nostrum vel nesciunt facilis.
+                    nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
+                    nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
+                    nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
+                    nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga nigga
                     </p>
                 </div>
 
                 <h3 class="header">
-                    Temple Precious Soye
+                    Saddam Hussein
                 </h3>
             </div>
 
