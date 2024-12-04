2148c6ee119b2359a8bbbb527943b182adc0a442
neilicatan <nhdrlicatan@usm.edu.ph>
2024-12-04 23:30:36 +0800

    modified:   lib/src/Login.php

--- lib/src/Login.php
+++ lib/src/Login.php
@@ -75,7 +75,7 @@ class Login
 
                     displayMessage("Login successful. You would be redirected to your dashboard shortly.", "text-green-500");
 
-                    header("Refresh: 3, /admin", true, 301);
+                    header("Refresh: 2, /admin", true, 301);
 
                     return;
                 } else {
